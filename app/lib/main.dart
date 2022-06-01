6af31d0aba7a66cab5ee33989ce5d7e60140ad81
Alexander Shilenkov <alexesc154@gmail.com>
2022-06-01 08:50:31 +0300

    Перенес виджеты в директорию

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:app/app.dart';
+import 'package:app/widget/app.dart';
 import 'package:flutter/material.dart';
 
 void main() {
--- app/lib/widget/app.dart
+++ app/lib/widget/app.dart
@@ -1,4 +1,4 @@
-import 'package:app/user_profile.dart';
+import 'package:app/widget/user_profile.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
