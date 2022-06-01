e8b75dbde070a6aa70c302c95d538e4ad8a7e5c0
Alexander Shilenkov <alexesc154@gmail.com>
2022-06-01 20:56:04 +0300

    Создал меню + добавил скролл

--- lib/widget/user_profile.dart
+++ lib/widget/user_profile.dart
@@ -1,40 +1,122 @@
-// ignore_for_file: sized_box_for_whitespace, prefer_const_constructors
+// ignore_for_file: sized_box_for_whitespace, must_be_immutable, use_key_in_widget_constructors, prefer_const_constructors
 
 import 'package:flutter/material.dart';
 
 class UserProfile extends StatelessWidget {
-  const UserProfile();
+  List<MenuRowData> firstMenuRow = [
+    MenuRowData(Icons.favorite, 'Избранное'),
+    MenuRowData(Icons.call, 'Звонки'),
+    MenuRowData(Icons.computer, 'Устройства'),
+    MenuRowData(Icons.folder, 'Папка с чатами'),
+  ];
+  List<MenuRowData> secondMenuRow = [
+    MenuRowData(Icons.notifications, 'Уведомления и звуки'),
+    MenuRowData(Icons.privacy_tip, 'Конфидицальность'),
+    MenuRowData(Icons.date_range, 'Данные и память'),
+    MenuRowData(Icons.brush, 'Оформление'),
+    MenuRowData(Icons.language, 'Язык'),
+  ];
+  UserProfile();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-            title: const Align(
-                child: Text("Настройки"), alignment: Alignment.center)),
-        body: Container(
-            width: double.infinity,
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: Text('Настройки'),
+      ),
+      body: Container(
+        width: double.infinity,
+        child: SingleChildScrollView(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              // ignore: prefer_const_literals_to_create_immutables
-              children: [
-                SizedBox(
-                  height: 30,
-                ),
-                _AvatarWidget(),
-                SizedBox(
-                  height: 30,
-                ),
-                _UserName(),
-                SizedBox(
-                  height: 10,
-                ),
-                _UserPhone(),
-                SizedBox(
-                  height: 10,
-                ),
-                _UserLogin(),
-              ],
-            )));
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            _UserInfo(),
+            SizedBox(height: 30),
+            _MenuWidget(menuRow: firstMenuRow),
+            SizedBox(height: 30),
+            _MenuWidget(menuRow: secondMenuRow),
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+class MenuRowData {
+  final IconData icon;
+  final String text;
+
+  MenuRowData(this.icon, this.text);
+}
+
+class _MenuWidget extends StatelessWidget {
+  final List<MenuRowData> menuRow;
+  const _MenuWidget({
+    Key? key,
+    required this.menuRow,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      child: Column(
+        children: menuRow.map((data) => _MenuWidgetRow(data: data)).toList(),
+      ),
+    );
+  }
+}
+
+class _MenuWidgetRow extends StatelessWidget {
+  final MenuRowData data;
+
+  const _MenuWidgetRow({
+    Key? key,
+    required this.data,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 10),
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        children: [
+          Icon(data.icon),
+          SizedBox(width: 15),
+          Expanded(child: Text(data.text)),
+          Icon(Icons.chevron_right_sharp),
+        ],
+      ),
+    );
+  }
+}
+
+class _UserInfo extends StatelessWidget {
+  const _UserInfo({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      width: double.infinity,
+      child: Column(
+        // ignore: prefer_const_literals_to_create_immutables
+        children: [
+          SizedBox(height: 30),
+          _AvatarWidget(),
+          SizedBox(height: 30),
+          _UserName(),
+          SizedBox(height: 10),
+          _UserPhone(),
+          SizedBox(height: 10),
+          _UserLogin(),
+        ],
+      ),
+    );
   }
 }
 
