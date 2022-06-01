888b65fee69a11f6bd0342de35d8209887b2a2b5
Alexander Shilenkov <alexesc154@gmail.com>
2022-06-01 08:38:49 +0300

    Создание экрана профиля, текст по центру
    Scaffold Center

--- app/lib/app.dart
+++ app/lib/app.dart
@@ -1,3 +1,4 @@
+import 'package:app/user_profile.dart';
 import 'package:flutter/material.dart';
 
 class App extends StatelessWidget {
@@ -6,7 +7,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Text("TEST"),
+      home: UserProfile(),
     );
   }
 }
---
+++ app/lib/user_profile.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class UserProfile extends StatelessWidget {
+  const UserProfile();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+        body: Center(
+      child: Text('TEST'),
+    ));
+  }
+}
