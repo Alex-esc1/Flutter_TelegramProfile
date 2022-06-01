ec0573f0d7956c5776223d3897342139c00e9cca
Alexander Shilenkov <alexesc154@gmail.com>
2022-06-01 08:42:08 +0300

    Титульник для AppBar

--- app/lib/user_profile.dart
+++ app/lib/user_profile.dart
@@ -6,7 +6,9 @@ class UserProfile extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          title: Text('Настройки'),
+        ),
         body: const Center(
           child: Text('TEST'),
         ));
